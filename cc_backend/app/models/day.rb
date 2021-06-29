b4294908b91bbce112be59f809fa3ca8a05f3f25
kjoos22 <kjoos22@gmail.com>
2021-06-29 14:56:56 -0400

    added validations for calories_expended

--- cc_backend/app/models/day.rb
+++ cc_backend/app/models/day.rb
@@ -2,9 +2,11 @@ class Day < ApplicationRecord
     belongs_to :person
     has_many :exercises
     validates :date, :pounds, :calories_consumed, 
-              :person_id, presence: true
+              :person_id, :calroies_expended, presence: true
     validates :pounds, numericality: {greater_than: 0}
     validates :calories_consumed, numericality: {only_integer: true,
+                                                 greater_than: 0}
+    validates :calories_expended, numericality: {only_integer: true,
                                                  greater_than: 0}
     validates :person_id, numericality: {only_integer: true, greater_than: 0}
 end
