446adf1a268299d133fe469a5d03dfa280c502f5
kjoos22 <kjoos22@gmail.com>
2021-07-01 18:35:22 -0400

    added directories to frontend src

--- cc_frontend/src/index.js
+++ cc_frontend/src/index.js
@@ -3,26 +3,18 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-
-
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-
 const enhancement = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-let reducer = '';
-
-let store = createStore(reducer, applyMiddleware(thunk))
-
+let store = createStore(reducer, enhancement(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
