d8c87942900c816fd5680b14967c6f1d9470fb80
kjoos22 <kjoos22@gmail.com>
2021-07-01 14:44:06 -0400

    configuring git ignore

--- cc_frontend/src/index.js
+++ cc_frontend/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+
+
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
+
+const enhancement = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let reducer = ''
+
+let store = createStore(reducer, applyMiddleware(thunk))
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
